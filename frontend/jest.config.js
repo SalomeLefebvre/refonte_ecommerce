248814b974c022e0497974bcd6631728a2896880
SalomeLefebvre <salo.lefebvre1@gmail.com>
2025-06-17 10:31:07 +0200

    ajout jest au frontend

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageReporters: ['html', 'text-summary'],
+};
